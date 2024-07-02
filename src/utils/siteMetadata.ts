d3a9bdedd1252d634f712bb60ad5f0007aaa3001
alannofrialdi <alannofrialdi098@gmail.com>
2024-07-02 16:38:23 +0700

    feat: add SEO

---
+++ src/utils/siteMetadata.ts
@@ -0,0 +1,14 @@
+const siteMetadata = {
+  title: "Alan Store",
+  author: "Alan Store",
+  headerTitle: "Alan Store",
+  description: "Alan Store provide you comfortable, quality, and cheap shoes",
+  language: "id-ID",
+  theme: "system",
+  siteUrl: "https://next-store-alpha-one.vercel.app/",
+  email: "alannofrialdi098@gmail.com",
+  locale: "id-ID",
+};
+
+export default siteMetadata;
+// module.exports = siteMetadata;
